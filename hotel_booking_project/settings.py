7732d25234169cb1f17a2e2fc1495557a355ddd5
noyon-jethitech <noyon.ahammad@jethitech.com>
2024-02-12 03:27:59 -0500

    cors policy added

--- hotel_booking_project/settings.py
+++ hotel_booking_project/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'hotel_app',
     'user_app',
